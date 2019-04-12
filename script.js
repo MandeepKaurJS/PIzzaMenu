8088f6d12f770386bb85b71870fd36421e7a1e97
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-04-11 18:52:12 -0700

    update html document and css file
    add some style for showText

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var text1 = document.getElementById("showtext2");
-var runningTotal = document.getElementById("totalPrice2");
+
  
  function getReceipt() {
 	// This initializes our string so it can get passed from  
 	// function to function, growing line by line into a full receipt
-	
+	var text1 = "";
+    var runningTotal = "";
 	var sizeTotal = 0;
 	var sizeArray = document.getElementsByClassName("size");
 	for (var i = 0; i < sizeArray.length; i++) {
 		if (sizeArray[i].checked) {
 			var selectedSize = sizeArray[i].value;
-			text1 = text1+selectedSize+"<br>";
+			text1 = text1+selectedSize +"<br>";
 		}
 	}
 	if (selectedSize === "Personal Pizza") {
@@ -24,10 +24,10 @@ var runningTotal = document.getElementById("totalPrice2");
 	}
 	runningTotal = sizeTotal;
 	console.log(selectedSize+" = $"+sizeTotal+".00");
-	console.log("size text1: "+text1);
-    console.log("subtotal: $"+runningTotal+".00");
+	console.log(text1);
+    console.log( "$"+runningTotal+".00");
     document.getElementById("showText2").innerHTML=text1;
-	document.getElementById("totalPrice2").innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+	document.getElementById("totalPrice2").innerHTML = "$"+runningTotal+".00";
     getMeat(runningTotal,text1);
     getCheese(runningTotal,text1);
     getSauce(runningTotal.text1);
@@ -57,8 +57,8 @@ var runningTotal = document.getElementById("totalPrice2");
 	console.log(meatCount+" meat - 1 free meat = "+"$"+meatTotal+".00");
 	console.log("meat text1: "+text1);
 	console.log("Purchase Total: "+"$"+runningTotal+".00");
-	document.getElementById("showText2").innerHTML=text1;
-	document.getElementById("totalPrice2").innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+    document.getElementById("showText2").innerHTML=text1;
+	document.getElementById("totalPrice2").innerHTML = "$"+runningTotal+".00";
 };	
 function getVeggies(){
     var vegTotal=0;
@@ -83,7 +83,7 @@ function getVeggies(){
     console.log("veg text1:"+text1);
     console.log("purchase Total:"+"$"+runningTotal+".00");
     document.getElementById("showtext2").innerHTML=text1;
-    document.getElementById("totalPrice2").innerHTML="<h3>Total: <Strong> $"+runningTotal+".00"+"</strong></h3>";
+    document.getElementById("totalPrice2").innerHTML="$"+runningTotal+".00"+"";
 };
 function getCheese(runningTotal,text1){
     var cheeseTotal=0;
